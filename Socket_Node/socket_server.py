b9c6e0bec8adbbb6a5e1210bc4244e7d54e29ee1
sunjianwen <sunjianwen.d@gmail.com>
2022-08-22 14:58:27 +0800

    Add files via upload

--- Socket_Node/socket_server.py
+++ Socket_Node/socket_server.py
@@ -25,22 +25,26 @@ class Server():
             data=self.client.recv(1024)
             if data:
                 hex_list=[i for i in data]
-                self.print_rece([hex(i) for i in data])
-                if hex_list[1]==0:
-                    self.main.car_flag=True
-                if hex_list[1]==1:
-                    rfid=int(hex_list[2])
-                    self.main.print_log('rfid is '+str(rfid))
-                    if self.main.local_path[-1] == rfid:
-                        self.main.rfid = rfid
-                        self.main.next_rfid = rfid
-                    else:
-                        for i in range(len(self.main.local_path)):
-                            # print(self.main.local_path[i],' ',rfid)
-                            if self.main.local_path[i] == rfid:
-                                self.main.next_rfid=self.main.local_path[i+1]
-                                self.main.rfid=rfid
-                                # print(self.main.next_rfid,self.main.rfid)
-                                break
+                if hex_list[1]==0 or hex_list[1]==1 or hex_list[1]==2 :
+                    self.print_rece([hex(i) for i in data])
+                    if hex_list[1]==0:
+                        self.main.car_flag=True
+                    if hex_list[1]==1:
+                        rfid=int(hex_list[2])
+                        self.main.print_log('rfid is '+str(rfid))
+                        if self.main.local_path[-1] == rfid:
+                            self.main.rfid = rfid
+                            self.main.next_rfid = rfid
+                        else:
+                            for i in range(len(self.main.local_path)):
+                                # print(self.main.local_path[i],' ',rfid)
+                                if self.main.local_path[i] == rfid:
+                                    self.main.next_rfid=self.main.local_path[i+1]
+                                    self.main.rfid=rfid
+                                    # print(self.main.next_rfid,self.main.rfid)
+                                    break
+                else:
+                    self.main.print_log('未知数据')
+                    print([hex(i) for i in data])
 if __name__ == '__main__':
     server=Server()
